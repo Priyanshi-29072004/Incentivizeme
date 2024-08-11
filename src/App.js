69c80535c197ac1ba61f74f10d30498cd11d3bda
Priyanshi Vaghasia <vaghasiapriyanshi@gmail.com>
2024-08-11 14:23:26 +0530

    updated name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to incentivizeme</div>;
+  return <div className="App">Welcome to incentivizeme test</div>;
 }
 
 export default App;
